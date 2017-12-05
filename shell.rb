035bd80f476a9e978e2d42a39a51533fbd8802ca
mic613 <mic613@gmail.com>
2017-12-05 04:12:28 -0500

    shell.rb added

---
+++ shell.rb
@@ -0,0 +1,15 @@
+# Define File Backup (fback) method
+def fback (fname)
+  fcont = File.read(fname)
+  File.new(#fcont)
+    end
+# Program start
+while TRUE
+  puts("Press q to quit or h for help")
+  user_input = gets.chomp
+  if user_input == 'q'  # Program quit condition
+    break
+  elsif user_input == 'h' # Program help screen (WIP)
+  puts ("help screen")
+  end
+end
